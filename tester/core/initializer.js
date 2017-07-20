8088d0f0cee35caf4a0badd4320e374b4bae1cbe
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-07-20 10:22:27 +0200

    mods

--- tester/core/initializer.js
+++ tester/core/initializer.js
@@ -3,35 +3,45 @@
 const express = require('express');
 const valkyrie = require('../../lambda/valkyrie/valkyrie');
 
-module.exports = (scenarioName) => {
+module.exports = (scenarioName) => new Promise((resolve, reject) => {
   let scenario;
   try {
     scenario = require(`../scenarios/${scenarioName}`);
   } catch (err) {
-    throw new Error('Scenario', scenarioName, 'not found;');
+    return reject('Scenario', scenarioName, 'not found;');
   }
-  const expressTest = scenario(express);
-  const valkyrieTest = scenario(valkyrie);
+  const expressApp = scenario(express);
+  const valkyrieApp = scenario(valkyrie);
 
-  expressTest.listen(6000, () => console.log('Express listening on port 6000'));
+  expressApp.listen(6000, () => {
+    resolve({
+      express: {
+        app: expressApp,
+        status: 'Express listening on port 6000'
+      },
+      valkyrie: {
+        app: valkyrieApp,
+        status: 'Valkyrie ready to be called',
+        call: (event) => new Promise((resolve, reject) => {
+          const _fail = (err) => reject(err);
+          const _succeed = (data) => resolve(data);
+          const _done = (err, data) => {
+            if (err) _fail(err);
+            else _succeed(data);
+          };
+          const context = {
+            fail: _fail,
+            succeed: _succeed,
+            done: _done
+          };
+          const callback = (err, data) => {
+            if (err) _fail(err);
+            else _succeed(data);
+          };
 
-  return module.exports = (event) => new Promise((resolve, reject) => {
-    const _fail = (err) => reject(err);
-    const _succeed = (data) => resolve(data);
-    const _done = (err, data) => {
-      if (err) _fail(err);
-      else _succeed(data);
-    };
-    const context = {
-      fail: _fail,
-      succeed: _succeed,
-      done: _done
-    };
-    const callback = (err, data) => {
-      if (err) _fail(err);
-      else _succeed(data);
-    };
-
-    return valkyrieTest.listen(event, context, callback);
+          return valkyrieApp.listen(event, context, callback);
+        })
+      }
+    });
   });
-};
+});
--- tester/core/tester.js
+++ tester/core/tester.js
@@ -3,54 +3,56 @@
 
 module.exports.startScenario = (scenarioName) => new Promise((resolve, reject) => {
   if (!scenarioName) return reject('You must specify a scenario name using flags -s, --scenario or writing it in the test file at "scenario" key;');
+  Promise.resolve(require('./initializer')(scenarioName))
+    .then(scenario => {
+      const bodyParser = require('body-parser');
+      const express = require('express');
+      const request = require('request');
+      const { htmlFormatter, jsonFormatter } = require('./formatter');
+      const app = express();
 
-  const bodyParser = require('body-parser');
-  const express = require('express');
-  const app = express();
-  const callLambda = require('./initializer')(scenarioName);
-  const request = require('request');
-  const { htmlFormatter, jsonFormatter } = require('./formatter');
+      app.use(bodyParser.json(), bodyParser.raw(), bodyParser.text(), bodyParser.urlencoded({ extended: false }));
 
-  app.use(bodyParser.json(), bodyParser.raw(), bodyParser.text(), bodyParser.urlencoded({ extended: false }));
+      app.use('*', (req, res) => {
+        const { headers, method, body, query, params, originalUrl } = req;
+        Promise.all([
+          new Promise((resolve, reject) => {
+            request({
+              url: `http://localhost:6000${originalUrl}`,
+              method,
+              headers
+            }, (error, response, body) => {
+              if (error) return reject(error);
+              resolve({
+                statusCode: response.statusCode,
+                headers: response.headers,
+                body
+              });
+            });
+          }),
+          scenario.valkyrie.call({
+            headers,
+            httpMethod: method,
+            body,
+            queryStringParams: query,
+            path: params[0]
+          })
+        ])
+          .then(data => {
+            //todo salvare files
+            res.send(htmlFormatter(data));
+          })
+          //todo gestire i fallimenti in modo chiaro
+          .catch(res.send);
+      });
 
-  app.use('*', (req, res) => {
-    delete req.headers['tests-format'];
-
-    const { headers, method, body, query, params, originalUrl } = req;
-    const event = {
-      headers: req.headers,
-      httpMethod: req.method,
-      body: req.body,
-      queryStringParams: req.query,
-      path: req.params[0]
-    };
-
-    Promise.all([
-      new Promise((resolve, reject) => {
-        request({
-          url: `http://localhost:6000${originalUrl}`,
-          method,
-          headers
-        }, (error, response, body) => {
-          if (error) return reject(error);
-          resolve({
-            statusCode: response.statusCode,
-            headers: response.headers,
-            body
-          });
-        });
-      }),
-      callLambda(event)
-    ])
-      .then(data => {
-        //todo salvare files
-        res.send(htmlFormatter(data));
-      })
-      .catch(res.send);
-  });
-
-  app.listen(8080, () => {
-    resolve(`${scenarioName} is listening on port 8080`);
-  });
+      app.listen(8080, () => resolve({
+        express: scenario.express,
+        valkyrie: scenario.valkyrie,
+        scenario: {
+          app,
+          status: `${scenarioName} is listening on port 8080`
+        }
+      }));
+    });
 });
-
