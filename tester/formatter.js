66aa0f0a6878fbbb61116b6b898eb8efc8b839f2
Nicolò Fuccella <nico.fuccella@gmail.com>
2017-07-20 09:53:15 +0200

    formatter init

--- tester/formatter.js
+++ tester/formatter.js
@@ -1,32 +1,13 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path')
+const style = fs.readFileSync(path.join(__dirname, '/templates/style.css'));
+
 module.exports.htmlFormatter = (data) => {
   return `
-    <style>
-      body {
-        font-family: monospace;
-        padding: 0;
-        border: 0;
-        outline: 0;
-        display: block;
-        text-align: center;
-      }
-      table {
-        margin-bottom: 20px;
-        margin-right: 20vw;
-        margin-left: 20vw;
-        table-layout: auto;
-        border-collapse: collapse;
-      }
-      
-      table, th, td {
-        border: 1px solid black;
-        border-bottom-left-radius: 3px;
-        border-bottom-right-radius: 3px;
-      }
-    </style>
     <div style="width: 50%">
-    <table>
+    <table class="nested-table">
       <tr>
         <th>Key</th>
         <th>Express</th>
@@ -39,6 +20,6 @@ module.exports.htmlFormatter = (data) => {
 
 function parseValue(value) {
   if (typeof value !== 'object') return value;
-  return `<table> ${ Object.entries(value).map(([key, value]) => `<tr><td>${key}</td><td>${value}</td></tr>`).join('')} </table>`;
+  return `<style>${style}</style><table class="parent-table"> ${ Object.entries(value).map(([key, value]) => `<tr><td>${key}</td><td>${value}</td></tr>`).join('')} </table>`;
 }
 
