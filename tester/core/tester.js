acc327bceb0d07d238d623c733dec9e835b57d5e
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-07-20 15:45:24 +0200

    mods

--- tester/core/tester.js
+++ tester/core/tester.js
@@ -46,11 +46,15 @@ const startScenario = (scenarioName) => new Promise((resolve, reject) => {
           })
         ])
           .then(data => {
+            //throw new Error('cazzi');
             res.send(htmlFormatter(Object.assign({}, { request: data[0].request, response: { express: data[0].response, valkyrie: data[1] } })));
           })
-          //todo gestire i fallimenti in modo chiaro
-          .catch(res.send);
+          .catch(err => {
+            res.send(err);
+            reject(err);
+          });
       });
+
 
       app.listen(8080, () => resolve({
         express: scenario.express,
