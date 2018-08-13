9ecdc632c5f50ac204bc2f69a87a59a4b2fa8375
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-08-13 13:16:14 +0200

    refactoring complete to 9.3

--- Valkyrie/lib/Utils.js
+++ Valkyrie/lib/Utils.js
@@ -1,23 +1,19 @@
-'use strict';
-
-
-//TODO: REVIEW
 const mime = require('mime');
 const contentType = require('content-type');
 const etag = require('etag');
 
 function _acceptParams(str, index) {
-  var parts = str.split(/ *; */);
-  var ret = { value: parts[0], quality: 1, params: {}, originalIndex: index };
+  const parts = str.split(/ *; */);
+  const ret = { value: parts[0], quality: 1, params: {}, originalIndex: index };
 
-  for (var i = 1; i < parts.length; ++i) {
-    var pms = parts[i].split(/ *= */);
+  ret.forEach(e => {
+    const pms = e.split(/ *= */);
     if ('q' === pms[0]) {
       ret.quality = parseFloat(pms[1]);
     } else {
       ret.params[pms[0]] = pms[1];
     }
-  }
+  });
 
   return ret;
 }
@@ -27,37 +23,23 @@ module.exports = class Utils {
     if (!type || !charset) {
       return type;
     }
-
-    // parse type
-    var parsed = contentType.parse(type);
-
-    // set charset
+    const parsed = contentType.parse(type);
     parsed.parameters.charset = charset;
-
-    // format type
     return contentType.format(parsed);
-  };
-
-  static forEach(arr, fn) {
-    let i = 0;
-    const len = arr.length;
-    while (i < len) {
-      fn (arr[i], i);
-      i++;
-    }
   }
 
-  static stringify(entity) {
+  static stringify(entity, replacer, spaces) {
     if (typeof entity === 'object') {
       try {
         const cache = [];
-        return JSON.stringify(entity, (key, value) => {
+
+        return JSON.stringify(entity, replacer || ((key, value) => {
           if (typeof value === 'object' && value !== null) {
             if (cache.indexOf(value) !== -1) return '[Circular]';
             cache.push(value);
           }
           return value;
-        });
+        }), spaces);
       } catch (err) {
         return String(entity);
       }
@@ -66,7 +48,6 @@ module.exports = class Utils {
     return String(entity);
   }
 
-
   static flatten(array) {
     if (!array) return [];
     return array.reduce(
@@ -74,32 +55,20 @@ module.exports = class Utils {
     );
   }
 
-  static repeatText(text, repetition) {
-    let out = '';
-    for (let i = 0; i < repetition; i++) out = `${out}${text}`;
-    return out;
+  static normalizeType(type) {
+    return ~type.indexOf('/') ?
+      _acceptParams(type) :
+      { value: mime.getType(type), params: {} };
   }
-
-  //TODO: REVIEW
-  static normalizeType(type){
-    return ~type.indexOf('/')
-      ? this._acceptParams(type)
-      : { value: mime.lookup(type), params: {} };
-  };
-
-  static normalizeTypes(types){
-    var ret = [];
-
-    for (var i = 0; i < types.length; ++i) {
-      ret.push(this.normalizeType(types[i]));
-    }
-
-    return ret;
-  };
 
   static etag(body, encoding) {
     const buf = !Buffer.isBuffer(body) ? new Buffer(body, encoding) : body;
     return etag(buf, { weak: false });
+  }
+
+  static wetag(body, encoding) {
+    const buf = !Buffer.isBuffer(body) ? new Buffer(body, encoding) : body;
+    return etag(buf, { weak: true });
   }
 
   static compileETag(val) {
@@ -126,10 +95,5 @@ module.exports = class Utils {
     }
 
     return fn;
-  }
-
-  static wetag(body, encoding) {
-    const buf = !Buffer.isBuffer(body) ? new Buffer(body, encoding) : body;
-    return etag(buf, { weak: true });
   }
 };
