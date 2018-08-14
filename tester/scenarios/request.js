ad253ddf93269b5bac1925ea0ec353415fcef6e6
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-08-14 15:56:12 +0200

    added request and response scenario

---
+++ tester/scenarios/request.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+module.exports = (engine, engineName) => {
+  const app = new engine();
+
+  app.all('/accepts/:type', (req, res) => {
+    res.send(req.accepts(req.params.type));
+  });
+
+  app.all('*', (req, res) => res.status(404).send('not found!'));
+
+  if (engineName === 'Valkyrie') console.log(app.describe());
+
+  return app;
+};
